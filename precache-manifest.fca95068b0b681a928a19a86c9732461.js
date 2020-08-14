8e1419319fc432daf7981c640cb71cec09a451e7
asathalmannan <ajmalhussainhy@gmail.com>
2020-08-14 07:05:09 +0530

    Updates

---
+++ precache-manifest.fca95068b0b681a928a19a86c9732461.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da27a07420b503ead1422dfd7158957b",
+    "url": "/stock-incharge/index.html"
+  },
+  {
+    "revision": "1c1dc3bb6c802bd573ba",
+    "url": "/stock-incharge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b02cdde0aa797a6ead7",
+    "url": "/stock-incharge/static/js/2.2beaf285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-incharge/static/js/2.2beaf285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1dc3bb6c802bd573ba",
+    "url": "/stock-incharge/static/js/main.afca9e3f.chunk.js"
+  },
+  {
+    "revision": "39d030eea999717b85b0",
+    "url": "/stock-incharge/static/js/runtime-main.15ca9d7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock-incharge/static/media/logo.5d5d9eef.svg"
+  }
+]);
